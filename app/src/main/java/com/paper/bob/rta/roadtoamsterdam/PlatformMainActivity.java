456a9abe7d405ff55fef6e9315ea66f7590f6859
bob99rum <htvgamer@gmail.com>
2019-01-25 20:45:10 +0100

    Initial commit

---
+++ app/src/main/java/com/paper/bob/rta/roadtoamsterdam/PlatformMainActivity.java
@@ -0,0 +1,13 @@
+package com.paper.bob.rta.roadtoamsterdam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlatformMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platform_main);
+    }
+}
