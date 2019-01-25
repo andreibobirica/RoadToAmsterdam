28aa5dc4093a8e944ff2f7625b37a12c7f474fc8
bob99rum <htvgamer@gmail.com>
2019-01-25 20:50:26 +0100

    Creazione della PLatformMainActivity.java

--- app/src/main/java/com/paper/bob/rta/roadtoamsterdam/PlatformMainActivity.java
+++ app/src/main/java/com/paper/bob/rta/roadtoamsterdam/PlatformMainActivity.java
@@ -2,12 +2,19 @@ package com.paper.bob.rta.roadtoamsterdam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PlatformMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Avvio della Main Activity in FullScrean
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Eliminazione Title BAR
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Activity del PLatform Game
         setContentView(R.layout.activity_platform_main);
     }
 }
