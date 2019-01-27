12e35056d3fab6f65a4affd0abe74b553d7b6fb5
bob99rum <htvgamer@gmail.com>
2019-01-27 23:46:04 +0100

    Implementazione Sperimentale dello schermo sempre acceso. Lo KEEP SCREAN ON

--- app/src/main/java/com/paper/bob/rta/roadtoamsterdam/PlatformMainActivity.java
+++ app/src/main/java/com/paper/bob/rta/roadtoamsterdam/PlatformMainActivity.java
@@ -1,5 +1,7 @@
 package com.paper.bob.rta.roadtoamsterdam;
 
+import android.content.Context;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,15 +11,24 @@ import android.widget.Toast;
 
 public class PlatformMainActivity extends AppCompatActivity {
 
+    protected PowerManager.WakeLock mWakeLock;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "My Tag");
+        this.mWakeLock.acquire();
+
         //Avvio della Main Activity in FullScrean
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //Eliminazione Title BAR
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Activity del PLatform Game
         setContentView(R.layout.activity_platform_main);
+
+
     }
     @Override
     public void onDestroy() {
