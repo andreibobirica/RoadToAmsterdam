75396c30db774950fe7f1766a5c029450d3a5f5d
bob99rum <htvgamer@gmail.com>
2019-02-08 20:15:46 +0100

    Keepscreen on effettivo abilitato e Miglioramento dataGraber

--- app/src/main/java/com/paper/bob/rta/roadtoamsterdam/gameUtils/DataGraber.java
+++ app/src/main/java/com/paper/bob/rta/roadtoamsterdam/gameUtils/DataGraber.java
@@ -72,93 +72,45 @@ public class DataGraber {
     }
 
 
-    public Background getBackground(String lvName)
+    private Node getLevel(String lvName)
     {
-        Background bg = null;
-        String bgName = "";
         NodeList livelli = radice.getChildNodes();
         for (int i = 0; i < livelli.getLength(); i++)
         {
             Node lv = livelli.item(i);
             if(lv.getAttributes().getNamedItem("name").getNodeValue().equals(lvName)) {
-                bgName = lv.getFirstChild().getNextSibling().getFirstChild().getTextContent();
-                break;
-            }else{}
-        }
-        int resId = context.getResources().getIdentifier(bgName, "drawable", context.getPackageName());
-        Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
-        bg = new Background(img,0,0);
-        Log.i("RTA", "Background");
-        return bg;
-    }
-
-    public ArrayList<Ostacolo> getOstacoli(String lvName)
-    {
-        ArrayList<Ostacolo> ostacoli = new ArrayList<Ostacolo>();
-        NodeList livelli = radice.getChildNodes();
-
-        for (int i = 0; i < livelli.getLength(); i++)
-        {
-            Node lv = livelli.item(i);
-            Log.i("RTA", "Nome lv: "+lv.getAttributes().getNamedItem("name").getNodeValue());
-            if(lv.getAttributes().getNamedItem("name").getNodeValue().equals(lvName)) {
-                NodeList ost = lv.getFirstChild().getChildNodes();
-                for (int e = 0; e < ost.getLength(); e++) {
-                    String imgName = ost.item(e).getFirstChild().getTextContent();
-                    int resId = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
-                    Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
-
-                    //Posizione
-                    int x = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("x").getNodeValue());
-                    int y = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("y").getNodeValue());
-
-                    //Grandezza Relativa
-                    String tipo = ost.item(e).getAttributes().getNamedItem("tipo").getNodeValue();
-                    int width,height;
-                    switch (tipo) {
-                        case "piccolo":
-                            width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_piccolo_width);
-                            height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_piccolo_height);
-                            break;
-                        case "normale":
-                            width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_width);
-                            height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_height);
-                            break;
-                        case "normale_oriz":
-                            width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_oriz_width);
-                            height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_oriz_height);
-                            break;
-                        case "grande":
-                            width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_grande_width);
-                            height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_grande_height);
-                            break;
-                        default:
-                            width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_width);
-                            height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_height);
-                            break;
-                    }
-                    //Individuazione n. frame
-                    int nFrame = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("frame").getNodeValue());
-                    //Individuazione se fisico
-                    boolean fisico = Boolean.parseBoolean(ost.item(e).getAttributes().getNamedItem("fisico").getNodeValue());
-                    //Adattamento della risoluzione relativa
-                    x = positionAdapter(x,y)[0];
-                    y = positionAdapter(x,y)[1];
-                    //Creazione Ostacolo
-                    Ostacolo o = new Ostacolo(img, x, y, height, width, nFrame);
-                    o.setFisico(fisico);
-                    ostacoli.add(o);
-                }
-                break;
+                return lv;
             }
         }
+        return livelli.item(0);
+    }
 
-/*
-        for(int i=0; i < ostacoli.size(); i++)
-        {Log.i("RTA", ostacoli.get(i).toString());}
-       */
-        Log.i("RTA", "  Ostacoli");
-        return ostacoli;
+    private int[] getGrandezza(String tipo)
+    {
+        int width,height;
+        switch (tipo) {
+            case "piccolo":
+                width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_piccolo_width);
+                height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_piccolo_height);
+                break;
+            case "normale":
+                width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_width);
+                height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_height);
+                break;
+            case "normale_oriz":
+                width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_oriz_width);
+                height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_oriz_height);
+                break;
+            case "grande":
+                width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_grande_width);
+                height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_grande_height);
+                break;
+            default:
+                width = context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_width);
+                height= context.getResources().getDimensionPixelSize(R.dimen.ostacolo_normale_height);
+                break;
+        }
+        return new int[]{width,height};
     }
 
     private int[] positionAdapter(int x, int y)
@@ -179,6 +131,47 @@ public class DataGraber {
         return  ret;
     }
 
+    public Background getBackground(String lvName)
+    {
+        int resId = context.getResources().getIdentifier(getLevel(lvName).getFirstChild().getNextSibling().getFirstChild().getTextContent(), "drawable", context.getPackageName());
+        Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
+        Background bg = new Background(img,0,0);
+        Log.i("RTA", "- Background");
+        return bg;
+    }
+
+    public ArrayList<Ostacolo> getOstacoli(String lvName)
+    {
+        ArrayList<Ostacolo> ostacoli = new ArrayList<Ostacolo>();
+        NodeList ost = getLevel(lvName).getFirstChild().getChildNodes();//Ostacoli Lista
+        for (int e = 0; e < ost.getLength(); e++) {
+            //IMAGE
+            int resId = context.getResources().getIdentifier(ost.item(e).getFirstChild().getTextContent(), "drawable", context.getPackageName());
+            Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
+            //Posizione
+            int x = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("x").getNodeValue());
+            int y = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("y").getNodeValue());
+            //Grandezza Relativa
+            String tipo = ost.item(e).getAttributes().getNamedItem("tipo").getNodeValue();
+            int width = getGrandezza(tipo)[0];
+            int height = getGrandezza(tipo)[1];
+            //Individuazione n. frame
+            int nFrame = Integer.parseInt(ost.item(e).getAttributes().getNamedItem("frame").getNodeValue());
+            //Individuazione se fisico
+            boolean fisico = Boolean.parseBoolean(ost.item(e).getAttributes().getNamedItem("fisico").getNodeValue());
+            //Adattamento della risoluzione relativa
+            x = positionAdapter(x,y)[0];
+            y = positionAdapter(x,y)[1];
+            //Creazione Ostacolo
+            Ostacolo o = new Ostacolo(img, x, y, height, width, nFrame);
+            o.setFisico(fisico);
+            ostacoli.add(o);
+        }
+
+        Log.i("RTA", "- Ostacoli");
+        return ostacoli;
+    }
+
     public Bitmap getNotifyImage()
     {
         int resId = context.getResources().getIdentifier("notify", "drawable", context.getPackageName());
@@ -189,107 +182,79 @@ public class DataGraber {
     public ArrayList<Personaggio> getPersonaggi(String lvName)
     {
         ArrayList<Personaggio> personaggi = new ArrayList<>();
-        NodeList livelli = radice.getChildNodes();
-        for (int i = 0; i < livelli.getLength(); i++)
-        {
-            Node lv = livelli.item(i);
-            if(lv.getAttributes().getNamedItem("name").getNodeValue().equals(lvName)) {
-                NodeList prs = lv.getFirstChild().getNextSibling().getNextSibling().getChildNodes();
-                for (int e = 0; e < prs.getLength(); e++) {
-                    String imgName = prs.item(e).getFirstChild().getTextContent();
-                    int resId = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
-                    Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
-                    //Posizione
-                    int x = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("x").getNodeValue());
-                    int y = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("y").getNodeValue());
-                    //Grandezza Relativa
-                    int width,height;
-                    width = context.getResources().getDimensionPixelSize(R.dimen.person_width);
-                    height= context.getResources().getDimensionPixelSize(R.dimen.person_height);
-                    //Individuazione n. frame
-                    int nFrame = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("frame").getNodeValue());
-                    //Adattamento della risoluzione relativa
-                    x = positionAdapter(x,y)[0];
-                    y = positionAdapter(x,y)[1];
-                    //Individuazione del dialogo
-                    String dialogo = prs.item(e).getAttributes().getNamedItem("dialogo").getNodeValue();
-                    //Individuazione notify
-                    boolean notify = Boolean.parseBoolean(prs.item(e).getAttributes().getNamedItem("notify").getNodeValue());
-                    //Creazione Personaggio
-                    Personaggio p = new Personaggio(img, x, y, height, width, nFrame,dialogo,notify);
-                    personaggi.add(p);
-                }
-
-            }
+        NodeList prs = getLevel(lvName).getFirstChild().getNextSibling().getNextSibling().getChildNodes();
+        for (int e = 0; e < prs.getLength(); e++) {
+            //Image
+            int resId = context.getResources().getIdentifier(prs.item(e).getFirstChild().getTextContent(), "drawable", context.getPackageName());
+            Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
+            //Posizione
+            int x = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("x").getNodeValue());
+            int y = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("y").getNodeValue());
+            //Grandezza Relativa
+            int width,height;
+            width = context.getResources().getDimensionPixelSize(R.dimen.person_width);
+            height= context.getResources().getDimensionPixelSize(R.dimen.person_height);
+            //Individuazione n. frame
+            int nFrame = Integer.parseInt(prs.item(e).getAttributes().getNamedItem("frame").getNodeValue());
+            //Adattamento della risoluzione relativa
+            x = positionAdapter(x,y)[0];
+            y = positionAdapter(x,y)[1];
+            //Individuazione del dialogo
+            String dialogo = prs.item(e).getAttributes().getNamedItem("dialogo").getNodeValue();
+            //Individuazione notify
+            boolean notify = Boolean.parseBoolean(prs.item(e).getAttributes().getNamedItem("notify").getNodeValue());
+            //Creazione Personaggio
+            Personaggio p = new Personaggio(img, x, y, height, width, nFrame,dialogo,notify);
+            personaggi.add(p);
         }
-        Log.i("RTA", "  Personaggi");
+        Log.i("RTA", "- Personaggi");
         return personaggi;
     }
 
     public Player getPlayer(String lvName)
     {
+        Node pl = getLevel(lvName).getFirstChild().getNextSibling().getNextSibling().getNextSibling();
+        //Estrapolazione immagini
+        //Immagine di base
+        String imgName = pl.getFirstChild().getTextContent();
+        int resId = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
+        Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
+        //IMMAGINI LEFT E RIGHT
+        int resIdL = context.getResources().getIdentifier(imgName+"left", "drawable", context.getPackageName());
+        Bitmap imgL = BitmapFactory.decodeResource(context.getResources(), resIdL);
+        int resIdR = context.getResources().getIdentifier(imgName+"right", "drawable", context.getPackageName());
+        Bitmap imgR = BitmapFactory.decodeResource(context.getResources(), resIdR);
+        //Posizione
+        int x = Integer.parseInt(pl.getAttributes().getNamedItem("x").getNodeValue());
+        int y = Integer.parseInt(pl.getAttributes().getNamedItem("y").getNodeValue());
+        //Grandezza Relativa
+        int width,height;
+        width = context.getResources().getDimensionPixelSize(R.dimen.person_width);
+        height= context.getResources().getDimensionPixelSize(R.dimen.person_height);
+        //Individuazione n. frame
+        int nFrame = Integer.parseInt(pl.getAttributes().getNamedItem("frame").getNodeValue());
+        //Adattamento della risoluzione relativa
+        x = positionAdapter(x,y)[0];
+        y = positionAdapter(x,y)[1];
+        //CREATING ANIMATION GIF
 
-        NodeList livelli = radice.getChildNodes();
-        Player play = null;
-        for (int i = 0; i < livelli.getLength(); i++)
-        {
-            Node lv = livelli.item(i);
-            if(lv.getAttributes().getNamedItem("name").getNodeValue().equals(lvName)) {
-                Node pl = lv.getFirstChild().getNextSibling().getNextSibling().getNextSibling();
-                //Estrapolazione immagini
-                //Immagine di base
-                String imgName = pl.getFirstChild().getTextContent();
-                int resId = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
-                Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
-                //IMMAGINI LEFT E RIGHT
-                int resIdL = context.getResources().getIdentifier(imgName+"left", "drawable", context.getPackageName());
-                Bitmap imgL = BitmapFactory.decodeResource(context.getResources(), resIdL);
-                int resIdR = context.getResources().getIdentifier(imgName+"right", "drawable", context.getPackageName());
-                Bitmap imgR = BitmapFactory.decodeResource(context.getResources(), resIdR);
-                //Posizione
-                int x = Integer.parseInt(pl.getAttributes().getNamedItem("x").getNodeValue());
-                int y = Integer.parseInt(pl.getAttributes().getNamedItem("y").getNodeValue());
-                //Grandezza Relativa
-                int width,height;
-                width = context.getResources().getDimensionPixelSize(R.dimen.person_width);
-                height= context.getResources().getDimensionPixelSize(R.dimen.person_height);
-                //Individuazione n. frame
-                int nFrame = Integer.parseInt(pl.getAttributes().getNamedItem("frame").getNodeValue());
-                //Adattamento della risoluzione relativa
-                x = positionAdapter(x,y)[0];
-                y = positionAdapter(x,y)[1];
-                //CREATING ANIMATION GIF
-
-                //Creazione Player
-                play = new Player(img,x,y,height,width,nFrame);
-                play.setJumpLAnim(imgL);
-                play.setJumpRAnim(imgR);
-                play.setLeftAnim(imgL);
-                play.setRightAnim(imgR);
-                break;
-            }
-        }
-        Log.i("RTA", "  Player");
+        //Creazione Player
+        Player play = new Player(img,x,y,height,width,nFrame);
+        play.setJumpLAnim(imgL);
+        play.setJumpRAnim(imgR);
+        play.setLeftAnim(imgL);
+        play.setRightAnim(imgR);
+        Log.i("RTA", "- Player");
         return play;
     }
 
     public Base getBase(String lvName)
     {
-        NodeList livelli = radice.getChildNodes();
-        Base base = null;
-        for (int i = 0; i < livelli.getLength(); i++)
-        {
-            Node lv = livelli.item(i);
-            if(lv.getAttributes().getNamedItem("name").getNodeValue().equals(lvName)) {
-                Node pl = lv.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getNextSibling();
-                String imgName = pl.getFirstChild().getTextContent();
-                int resId = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
-                Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
-                base = new Base(img);
-                break;
-            }
-        }
-        Log.i("RTA", "  Base");
+        Node pl = getLevel(lvName).getFirstChild().getNextSibling().getNextSibling().getNextSibling().getNextSibling();
+        int resId = context.getResources().getIdentifier(pl.getFirstChild().getTextContent(), "drawable", context.getPackageName());
+        Bitmap img = BitmapFactory.decodeResource(context.getResources(), resId);
+        Base base = new Base(img);
+        Log.i("RTA", "- Base");
         return base;
     }
 
